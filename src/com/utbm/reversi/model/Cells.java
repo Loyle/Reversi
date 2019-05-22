30577f0d97af17caeaea43ca12fa6e689070a69b
Loyle <antoine.charmeau@laposte.net>
2019-05-22 17:59:08 +0200

    Create Cells.java

---
+++ src/com/utbm/reversi/model/Cells.java
@@ -0,0 +1,17 @@
+package com.utbm.reversi.model;
+
+public class Cells {
+	private int value;
+	
+	public Cells() {
+		this.value = 0;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
